5ffc58b2b936d175d8c393029f790d837deb8833
PameGiribone <pame.giribone@hotmail.com>
2024-12-02 20:57:13 -0300

    actualizacion de credenciales

--- src/main/java/com/backend/mundoAnimal/config/AppConfig.java
+++ src/main/java/com/backend/mundoAnimal/config/AppConfig.java
@@ -1,12 +1,13 @@
 package com.backend.mundoAnimal.config;
 
-import com.backend.mundoAnimal.entity.Usuario;
-import com.backend.mundoAnimal.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.backend.mundoAnimal.entity.Usuario;
+import com.backend.mundoAnimal.repository.UsuarioRepository;
 
 @Configuration
 public class AppConfig {
@@ -20,10 +21,10 @@ public class AppConfig {
     @Bean
     public ApplicationRunner initializer() {
         return args -> {
-            if (usuarioRepository.findByEmail("admin@example.com") == null) {
+            if (usuarioRepository.findByEmail("petshopvalu@store.com") == null) {
                 Usuario admin = new Usuario();
-                admin.setEmail("admin@example.com");
-                admin.setPassword(passwordEncoder.encode("adminpassword"));
+                admin.setEmail("petshopvalu@store.com");
+                admin.setPassword(passwordEncoder.encode("petshopValu24"));
                 admin.setRole("ROLE_ADMIN");
                 usuarioRepository.save(admin);
             }
